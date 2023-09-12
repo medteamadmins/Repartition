58a75b2e5d2c2d33b377100b35d4e26305b65f2a
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-09-12 15:11:08 -0400

    Add files via upload
    This script Resizes the OS partition and makes another partition and volume D.

---
+++ Repartition.ps1
@@ -0,0 +1,4 @@
+Resize-Partition -DriveLetter C -Size 250GB
+New-Partition -DiskNumber 0 -UseMaximumSize -NewDriveLetter D | Format-Volume
+Set-Volume -DriveLetter D -NewFileSystemLabel "Data"
+New-Item C:\Partitionlog.txt
