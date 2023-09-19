9f23b7ec45bdca12912345380d14d9601ef48766
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-09-19 14:17:51 -0400

    Update Repartition.ps1
    Changed partition size from 250GB to 50%

--- Repartition.ps1
+++ Repartition.ps1
@@ -1,3 +1,8 @@
-Resize-Partition -DriveLetter C -Size 250GB
+# Calculate the new size as 50% of the current size
+$currentSize = (Get-Partition -DriveLetter C).Size
+$newSize = [math]::Round($currentSize / 2)
+
+# Resize the C drive to the new size
+Resize-Partition -DriveLetter C -Size $newSize
 New-Partition -DiskNumber 0 -UseMaximumSize -NewDriveLetter D | Format-Volume
 Set-Volume -DriveLetter D -NewFileSystemLabel "Data"
